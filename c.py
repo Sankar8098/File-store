4abed874ae338297120d1b4b041a0ce136e440ec
bavabee <93858110+bavabee@users.noreply.github.com>
2022-11-09 20:03:53 +0530

    Update c.py

--- c.py
+++ c.py
@@ -4,11 +4,11 @@ import os
 
 APP_ID = os.environ.get("APP_ID", "9840531")
 APP_HASH = os.environ.get("APP_HASH", "2ac10377616df23907e134df14bdf08b")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5636865368:AAH2pa9VZRHOQwyUvGhd82OI_LISVq7HDfI")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5315024778:AAHWRzXII0ea8E79No0xcS5M-DkvvHU0JOI")
 DB_URI = os.environ.get("DB_URI", "mongodb+srv://Bave999:Bave999@cluster0.1aheaa1.mongodb.net/?retryWrites=true&w=majority")
 '''
 APP_ID = 9840531
 APP_HASH = "2ac10377616df23907e134df14bdf08b"
-BOT_TOKEN = "5636865368:AAH2pa9VZRHOQwyUvGhd82OI_LISVq7HDfI"
+BOT_TOKEN = "5315024778:AAHWRzXII0ea8E79No0xcS5M-DkvvHU0JOI"
 DB_URI = "mongodb+srv://Bave999:Bave999@cluster0.1aheaa1.mongodb.net/?retryWrites=true&w=majority"
 '''
